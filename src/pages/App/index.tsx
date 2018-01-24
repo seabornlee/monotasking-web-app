a5ad498c8e362b73db17a09ffa68ce49f8b632f2
TylerChang <TylerChangMore@gmail.com>
2018-01-24 21:40:06 +0800

    [Style] Beautify style

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -4,18 +4,25 @@ import { Button } from '../../components/Button'
 
 import './index.scss'
 
+const logo = require('../../assets/images/mono-logo-black.png')
+
 interface Props {
   children: React.ReactNode
 }
 
 export const App: React.StatelessComponent<Props> = (props: Props) => (
   <div className='app-container'>
-    <div className='input-group'>
-      <input className='new-task' type='text' placeholder='添加 #接下来最应该开始的工作#'/>
-      <Button type='primary' className='btn-add-task'><i className='icon icons-download'/></Button>
+    <header>
+      <img src={logo} alt='MonoToday'/>
+    </header>
+    <div className='new-task'>
+      <input className='new-task-input' type='text' placeholder='添加 #接下来最应该开始的工作#'/>
+      <Button type='primary' className='new-task-btn'><i className='icon icons-download'/></Button>
     </div>
     {props.children}
-    <button>开始单核</button>
-    <a href='#'>关于单核工作法</a>
+    <Button type='primary' className='start-mono-btn'>开始单核</Button>
+    <footer>
+      <a href='https://baike.baidu.com/item/%E5%8D%95%E6%A0%B8%E5%B7%A5%E4%BD%9C%E6%B3%95/22119461?fr=aladdin'>什么是单核工作法？</a>
+    </footer>
   </div>
 )
