dbca610e3c7fdcc29c3b22076f5562b3e5c2eac8
TylerChang <TylerChangMore@gmail.com>
2018-01-24 21:58:26 +0800

    Add Tasks component

--- src/pages/QuickList/index.tsx
+++ src/pages/QuickList/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
+import { Tasks } from '../Tasks'
 
 interface Props {}
 
 export const QuickList: React.StatelessComponent<Props> = (props: Props) => (
-  <div>
-    Quick List
-  </div>
+  <Tasks/>
 )
---
+++ src/pages/Tasks/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {}
+
+export const Tasks: React.StatelessComponent<Props> = (props: Props) => (
+  <div>
+    Tasks
+  </div>
+)
