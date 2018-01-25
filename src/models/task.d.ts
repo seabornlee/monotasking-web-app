a936974598541cab450b9e3f0f35770c64dfec2c
TylerChang <TylerChangMore@gmail.com>
2018-01-25 23:31:50 +0800

    Update icons and adjust task list layout

--- src/models/task.d.ts
+++ src/models/task.d.ts
@@ -2,6 +2,7 @@ declare interface Task {
   id: number
   queue: string
   title: string
+  completed_at: string
 }
 
 declare type Tasks = Task[]
--- src/pages/TaskList/index.tsx
+++ src/pages/TaskList/index.tsx
@@ -10,23 +10,44 @@ class TaskList extends React.Component<Props> {
   public render () {
     return (
       <ul className='task-list'>
-        {
-          this.props.tasks.map((task: Task) => {
-            return (
-              <li key={task.id}>
-                <span className='task-title'>{task.title}</span>
-                <div className='actions'>
-                  <i className='iconfont icon-roundcheck' />
-                  <i className='iconfont icon-pullleft' />
-                  <i className='iconfont icon-pullright' />
-                  <i className='iconfont icon-delete' />
-                </div>
-              </li>
-            )
-          })
-        }
+        {this.props.tasks.map((task: Task) => this.renderTask(task))}
       </ul>
     )
+  }
+
+  private renderTask = (task: Task) => {
+    return (
+      <li key={task.id}>
+        {
+          this.isCompleted(task) ? <i className='iconfont icon-roundcheck' /> : ''
+        }
+        <span className='task-title'>{task.title}</span>
+        <div className='actions'>
+          {
+            !this.isCompleted(task) ? <i className='iconfont icon-roundcheck' /> : ''
+          }
+          {
+            this.isInGrassCatcher(task) ? <i className='iconfont icon-pullleft' /> : ''
+          }
+          {
+            this.isInQuickList(task) ? <i className='iconfont icon-pullright' /> : ''
+          }
+          <i className='iconfont icon-delete' />
+        </div>
+      </li>
+    )
+  }
+
+  private isInQuickList = (task: Task) => {
+    return task.queue === 'quick' && task.completed_at === null
+  }
+
+  private isInGrassCatcher = (task: Task) => {
+    return task.queue === 'grass_catcher' && task.completed_at === null
+  }
+
+  private isCompleted = (task: Task) => {
+    return task.completed_at !== null
   }
 }
 
